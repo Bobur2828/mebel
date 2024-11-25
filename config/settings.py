09d0c5bb17cb264838f6e2805037f05f9590c9d0
Saidanasxon <saydullayevsaidanasxon@gmail.com>
2024-11-25 18:17:18 +0500

    jazzmin qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ ALLOWED_HOSTS = ['*']
 # ============================INSTALLED_APPS============================
 
 DJANGO_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
